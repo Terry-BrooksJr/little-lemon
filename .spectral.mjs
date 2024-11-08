2879e3cb197b72893dbfefd5c81d04b06b273ac8
Terry Arthur Brooks, Jr. <Terry.Arthur@BrooksJr.com>
2024-11-08 15:44:58 -0500

    Added 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI2Mzc5OQ/spectral.js?branch_slug=master&token=d5bf7638-c203-4258-9167-96deb86daaad";
+export default { extends: ruleset };
